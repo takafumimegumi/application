18a38f3f584e32e6f5f300d1844bbbed2d99379d
Takafumi Megumi <me@takafumimegumi.com>
2020-03-06 18:28:31 +0900

    Controller抽象クラスを作成し、コンストラクタとアクションを実行するrun()メソッドを定義

---
+++ core/Controller.php
@@ -0,0 +1,38 @@
+<?php
+
+abstract class Controller {
+
+    protected $controller_name;
+    protected $action_name;
+    protected $application;
+    protected $request;
+    protected $response;
+    protected $session;
+    protected $db_manager;
+
+    public function __construct($application) {
+        //（例）'UserCntoroller' → 'user'
+        $this->controller_name = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application = $application;
+        $this->request = $application->getRequest();
+        $this->response = $application->getResponse();
+        $this->session = $application->getSession();
+        $this->db_manager = $application->getDbManager();
+    }
+
+    public function run($action, $params = []) {
+        $this->action_name = $action;
+
+        $action_method = $action . 'Action';
+        if (!method_exists($this, $action_method)) {
+            $this->forward404();
+        }
+
+        // 可変関数の仕組みを使ってアクションを特定し、存在すれば実行
+        $content = $this->$action_method($params);
+
+        return $content;
+    }
+
+}
